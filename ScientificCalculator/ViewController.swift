5668bc5e49b5c5bab2b0fd36bc1fbc929f28a868
Alex Hannigan <alexhannigan@AlexnoMacBook-Air.local>
2017-10-20 11:35:09 +0100

    Initial Commit

---
+++ ScientificCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScientificCalculator
+//
+//  Created by Alex Hannigan on 2017/10/20.
+//  Copyright © 2017年 Alex Hannigan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
